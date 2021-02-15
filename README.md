c2b9514d0335335ac06a1d664f056a9727bad6e7
Anastasios Ioakimidis <anastasios.ioakimidis@intrasoft-intl.com>
2021-02-15 17:42:12 +0200

    Refactored some var names. Updated readme file. Modified some file locations

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-"# CovidTrackerGreece" 
+# CovidTrackerGreece
 
-A small app written in Python that reads API data for COVID-19 cases in Greece and displays them on a map.
+
+
+CovidTrackerGreece is a small app written in Python that reads API data for COVID-19 cases in Greece and displays them on a map.
+
+## Requirements 
+
+You need Python 3.5 or later installed on your machine. 
+
+For other Linux flavors, macOS and Windows, packages are available at
+
+[https://www.python.org/getit](https://www.python.org/getit)
+
+
+## Usage
+Clone the git repository to a location of your choice.
+
+
+```python
+git clone https://github.com/A-Ioakimidis/CovidTrackerGreece.git
+```
+
+Enter the directory where the application was downloaded and navidate to the src folder.
+```python
+cd {project_dir}/src
+```
+
+Open a command prompt, and run the following:
+```python
+python app.py
+```
+
+The application should run, and a browser window will open displaying the map.
--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
+import os
+import webbrowser
+
 import folium
 import requests
 
@@ -6,17 +9,17 @@ import requests
 def create_request_and_response():
     try:
         response = requests.get('https://covid-19-greece.herokuapp.com/regions')
-        data = response.json();
+        data = response.json()
         return data
     except:
-        print("An error occured while retrieving data")
+        print("An error occurred while retrieving data")
 
 
 # Creates the folium map
 def create_map():
-    map = folium.Map(location=[39.0742, 21.8243], zoom_start=7, tiles="cartodbpositron")
-    map.save("CovidMapGreece.html")
-    return map
+    covid_map = folium.Map(location=[39.0742, 21.8243], zoom_start=7, tiles="cartodbpositron")
+    covid_map.save("CovidMapGreece.html")
+    return covid_map
 
 
 # Add details to popup marker
@@ -26,18 +29,24 @@ def add_popup_details(details):
 
 
 # Adds the markers to the map. Displays total cases for each major city
-def add_markers_to_map(data, map):
+def add_markers_to_map(data, covid_map):
     feature_group = folium.FeatureGroup(name='CovidMapGreece')
     for coords in data['regions']:
         feature_group.add_child(
             folium.Marker(location=[coords['latitude'], coords['longtitude']], popup=add_popup_details(coords),
                           icon=folium.Icon(color='red')))
-        map.add_child(feature_group)
-        map.save("CovidMapGreece.html")
+        covid_map.add_child(feature_group)
+        covid_map.save("CovidMapGreece.html")
         str(coords['total_cases'])
+
+
+def open_map_from_html():
+    filename = 'file:///' + os.getcwd() + '/' + 'CovidMapGreece.html'
+    webbrowser.open_new_tab(filename)
 
 
 # Call the application
 api_data = create_request_and_response()
 created_map = create_map()
 add_markers_to_map(api_data, created_map)
+open_map_from_html()
