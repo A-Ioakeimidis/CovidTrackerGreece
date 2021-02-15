4dd80f55778c6bf783f7e40fb621a53db8d4a889
Anastasios Ioakimidis <anastasios.ioakimidis@intrasoft-intl.com>
2021-02-15 10:50:57 +0200

    Minor changes to markers. Added code in src folder

---
+++ src/app.py
@@ -0,0 +1,43 @@
+import folium
+import requests
+
+
+# Creates the request and Json response
+def create_request_and_response():
+    try:
+        response = requests.get('https://covid-19-greece.herokuapp.com/regions')
+        data = response.json()
+        return data
+    except:
+        print("An error occured while retrieving data")
+
+
+# Creates the folium map
+def create_map():
+    map = folium.Map(location=[39.0742, 21.8243], zoom_start=7, tiles="cartodbpositron")
+    map.save("CovidMapGreece.html")
+    return map
+
+
+# Add details to popup marker
+def add_popup_details(details):
+    full_details = "Cases : {} \n Population: {}".format(details['total_cases'], details['population'])
+    return full_details
+
+
+# Adds the markers to the map. Displays total cases for each major city
+def add_markers_to_map(data, map):
+    feature_group = folium.FeatureGroup(name='CovidMapGreece')
+    for coords in data['regions']:
+        feature_group.add_child(
+            folium.Marker(location=[coords['latitude'], coords['longtitude']], popup=add_popup_details(coords),
+                          icon=folium.Icon(color='red')))
+        map.add_child(feature_group)
+        map.save("CovidMapGreece.html")
+        str(coords['total_cases'])
+
+
+# Call the application
+api_data = create_request_and_response()
+created_map = create_map()
+add_markers_to_map(api_data, created_map)
